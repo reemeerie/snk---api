124d0bc74ee4c9f435412fbfb7b5d2eae249f9ab
thomasReemeerie <thomasreemeerie0@gmail.com>
2022-10-14 14:54:51 -0300

    SNK API BETA

---
+++ src/v1/rutas/rutasZapatillas.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const controladorZapatillas = require("../../controladores/controladorZapatillas")
+
+router.get('/', controladorZapatillas.getAllZapas)
+
+router.get('/:id', controladorZapatillas.getUnaZapa)
+
+router.post('/', controladorZapatillas.creoUnaZapa)
+
+router.patch('/:id', controladorZapatillas.editoUnaZapa)
+
+router.delete('/:id', controladorZapatillas.borroUnaZapa)
+
+
+module.exports = router;
