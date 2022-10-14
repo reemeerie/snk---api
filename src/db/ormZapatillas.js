ebe4fef735f64d049508626a0ffd40aeea45ad13
thomasReemeerie <thomasreemeerie0@gmail.com>
2022-10-14 14:54:51 -0300

    SNK API BETA

---
+++ src/db/ormZapatillas.js
@@ -0,0 +1,59 @@
+const zapatillas = require("./zapatillas.json")
+const utils  = require("./utils")
+
+const getAllZapatillas = () => {
+    return zapatillas;
+}
+
+const crearZapatilla = (zapaNueva) => {
+    const isAlreadyAdded = zapatillas.find((zapa) => zapa.nombre == zapaNueva.nombre)
+    if (isAlreadyAdded) {
+        return;
+    }
+    zapatillas.push(zapaNueva)
+    utils.saveToDatabase(zapatillas)
+    return zapaNueva
+}
+
+const getUnaZapa = (zapaId) => {
+    const zapatilla = zapatillas.find((zapa) => zapa.id == zapaId);
+    if (!zapatilla) {
+      return;
+    }
+    return zapatilla;
+};
+
+const editoUnaZapa = (id, changes) => {
+    const indexAActualizar = zapatillas.findIndex(
+      (zapa) => zapa.id === id
+    );
+    if (indexAActualizar === -1) {
+      return;
+    }
+    const zapaEditada = {
+      ...zapatillas[indexAActualizar],
+      ...changes
+    };
+    zapatillas[indexAActualizar] = zapaEditada;
+    utils.saveToDatabase(zapatillas);
+    return zapaEditada;
+  };
+  
+  const borroUnaZapa = (id) => {
+    const indexABorrar = zapatillas.findIndex(
+      (zapa) => zapa.id === id
+    );
+    if (indexABorrar === -1) {
+      return;
+    }
+    zapatillas.splice(indexABorrar, 1);
+    utils.saveToDatabase(zapatillas);
+  };
+
+module.exports = {
+    getAllZapatillas,
+    crearZapatilla,
+    getUnaZapa,
+    editoUnaZapa,
+    borroUnaZapa
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const PORT = process.env.PORT || 5000;
+const zapatillasRouterV1 = require("./v1/rutas/rutasZapatillas")
+
+app.use(cors())
+app.use(express.json())
+app.use('/public', express.static('src/public'))
+app.use("/api/v1/zapatillas", zapatillasRouterV1);
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
