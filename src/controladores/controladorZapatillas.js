f8642fd0a5699eb63a05f6fe096a35a3b4d5cd33
Maximiliano Bianco <mbianco@LC0922-VKYLVPV6MH.local>
2022-10-16 17:07:02 -0300

    mongo corregido

--- src/controladores/controladorZapatillas.js
+++ src/controladores/controladorZapatillas.js
@@ -1,64 +1,64 @@
-const servZapatillas = require("../servicios/servZapatillas")
+const servZapatillas = require('../servicios/servZapatillas')
 
-const getAllZapas = (req,res) => {
-    try {
-        const allZapas = servZapatillas.getAllZapas()
-        return res.json({ status:"OK", data: allZapas })
-    } catch (error) {
-        console.error(error)
-    }
+const getAllZapas = async (req, res) => {
+  try {
+    const allZapas = await servZapatillas.getAllZapas()
+    return res.json({ status: 'OK', data: allZapas })
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-const getUnaZapa = (req,res) => {
-    const { params: { id } } = req;
-      if (!id) {
-        return;
-      }
-      const zapa = servZapatillas.getUnaZapa(id);
-      res.send({ status: "OK", data: zapa });
+const getUnaZapa = (req, res) => {
+  const { params: { id } } = req
+  if (!id) {
+    return
+  }
+  const zapa = servZapatillas.getUnaZapa(id)
+  res.send({ status: 'OK', data: zapa })
 }
 
-const creoUnaZapa = (req,res) => {
-    const request = req.body
-    if (!request.id || !request.nombre || !request.marca || !request.img || !request.precio || !request.stock) {
-        return;
-    }
+const creoUnaZapa = async (req, res) => {
+  const request = req.body
+  if (!request.id || !request.nombre || !request.marca || !request.img || !request.precio || !request.stock) {
+    return
+  }
 
-    const nuevaZapa = {
-        id : request.id,
-        nombre : request.nombre,
-        marca : request.marca,
-        img : request.img,
-        precio : request.precio,
-        stock : request.stock
-    }
+  const nuevaZapa = {
+    id: request.id,
+    nombre: request.nombre,
+    marca: request.marca,
+    img: request.img,
+    precio: request.precio,
+    stock: request.stock
+  }
 
-    const zapaCreada = servZapatillas.creoUnaZapa(nuevaZapa)
-    res.status(201).send({ status: "OK", data: zapaCreada})
+  const zapaCreada = await servZapatillas.creoUnaZapa(nuevaZapa)
+  res.status(201).send({ status: 'OK', data: zapaCreada })
 }
 
-const editoUnaZapa = (req,res) => {
-    const { body, params: { id } } = req;
-      if (!id) {
-        return;
-      }
-      const zapaEditada = servZapatillas.editoUnaZapa(id, body);
-      res.send({ status: "OK", data: zapaEditada });
+const editoUnaZapa = (req, res) => {
+  const { body, params: { id } } = req
+  if (!id) {
+    return
+  }
+  const zapaEditada = servZapatillas.editoUnaZapa(id, body)
+  res.send({ status: 'OK', data: zapaEditada })
 }
 
-const borroUnaZapa = (req,res) => {
-    const { params: { id }} = req;
-      if (!id) {
-        return;
-      }
-      servZapatillas.borroUnaZapa(id);
-      res.status(204).send({ status: "OK" });
+const borroUnaZapa = (req, res) => {
+  const { params: { id } } = req
+  if (!id) {
+    return
+  }
+  servZapatillas.borroUnaZapa(id)
+  res.status(204).send({ status: 'OK' })
 }
 
 module.exports = {
-    getAllZapas,
-    getUnaZapa,
-    creoUnaZapa,
-    editoUnaZapa,
-    borroUnaZapa
+  getAllZapas,
+  getUnaZapa,
+  creoUnaZapa,
+  editoUnaZapa,
+  borroUnaZapa
 }
--- src/db/connection.js
+++ src/db/connection.js
@@ -1,12 +1,11 @@
-const mongoose = require("mongoose")
-const connectionString = "mongodb+srv://thomasReemeerie:yerbamate41@thesneakersstore.7n0a7ep.mongodb.net/?retryWrites=true&w=majority"
+const mongoose = require('mongoose')
+const connectionString = 'mongodb+srv://thomasReemeerie:yerbamate41@thesneakersstore.7n0a7ep.mongodb.net/snk-api?retryWrites=true&w=majority'
 
 mongoose.connect(connectionString)
-.then(() => {
-    console.log("Database connected")
-}).catch( err => {
-    console.error(err);
-})
+  .then(() => {
+    console.log('Database connected')
+  }).catch(err => {
+    console.error(err)
+  })
 
 module.exports
-
