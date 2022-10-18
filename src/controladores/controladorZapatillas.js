f6ec0a1d11d78a5c2040f1d626215636457cea42
thomasReemeerie <thomasreemeerie0@gmail.com>
2022-10-18 14:37:40 -0300

    CRUD Funcionando

--- src/controladores/controladorZapatillas.js
+++ src/controladores/controladorZapatillas.js
@@ -11,7 +11,9 @@ const getAllZapas = async (req, res) => {
 
 const getUnaZapa = async (req, res) => {
   const { params: { id } } = req
-  if (!id) {
+  const newZapaId = Number(id)
+  if (isNaN(newZapaId)) {
+    res.status(404).send({ warning: 'Tiene que ser un numero' })
     return
   }
   const zapa = await servZapatillas.getUnaZapa(id)
@@ -20,7 +22,9 @@ const getUnaZapa = async (req, res) => {
 
 const creoUnaZapa = async (req, res) => {
   const request = req.body
+
   if (!request.id || !request.nombre || !request.marca || !request.img || !request.precio || !request.stock) {
+    res.status(404).send({ warning: 'Faltan campos obligatorios' })
     return
   }
 
@@ -32,27 +36,36 @@ const creoUnaZapa = async (req, res) => {
     precio: request.precio,
     stock: request.stock
   }
-
   const zapaCreada = await servZapatillas.creoUnaZapa(nuevaZapa)
   res.status(201).send({ status: 'OK', data: zapaCreada })
 }
 
 const editoUnaZapa = async (req, res) => {
-  const { body, params: { id } } = req
-  if (!id) {
+  const changes = req.body
+  const zapaId = req.params.id
+
+  const newZapaId = Number(zapaId)
+
+  if (isNaN(newZapaId)) {
+    res.status(404).send({ warning: 'Tiene que ser un numero' })
     return
   }
-  const zapaEditada = await servZapatillas.editoUnaZapa(id, body)
+  const zapaEditada = await servZapatillas.editoUnaZapa(newZapaId, changes)
   res.send({ status: 'OK', data: zapaEditada })
 }
 
 const borroUnaZapa = async (req, res) => {
-  const { params: { id } } = req
-  if (!id) {
+  const zapaId = req.params.id
+
+  const newZapaId = Number(zapaId)
+
+  if (isNaN(newZapaId)) {
+    res.status(404).send({ warning: 'Tiene que ser un numero' })
     return
   }
-  const zapaBorrada = await servZapatillas.borroUnaZapa(id)
-  res.status(204).send({ status: 'OK', data: zapaBorrada })
+
+  const zapaBorrada = await servZapatillas.borroUnaZapa(newZapaId)
+  res.status(410).send({ status: 'OK', data: zapaBorrada })
 }
 
 module.exports = {
--- src/db/utils.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-const saveToDatabase = (DB) => {
-  fs.writeFileSync('./src/database/zapatillas.json',
-    JSON.stringify(DB),
-    { encoding: 'utf-8' })
-}
-
-module.exports = { saveToDatabase }
